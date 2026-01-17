1e14be34a9b37b900a46817ef5d0603dcbe73321
OperativnayaLinia <tyrkeytest84@gmail.com>
2026-01-17 19:38:25 +0300

    move index.html to public and serve it

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Роут для главной страницы
+// раздаём статику
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.get('/', (req, res) => {
-  res.send('Сервер работает 🚀');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// ВАЖНО: используем процесс.env.PORT для Railway
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Сервер запущен на порту ${PORT}`);
